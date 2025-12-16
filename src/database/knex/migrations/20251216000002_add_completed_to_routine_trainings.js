4a9d28844919ff786008eca58b95adc7cabc05b7
SamuelSmk <samu.charlles3@gmail.com>
2025-12-16 16:51:09 -0300

    atualizações back

---
+++ src/database/knex/migrations/20251216000002_add_completed_to_routine_trainings.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.alterTable('routine_trainings', function(table) {
+    table.boolean('completed').defaultTo(false);
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.alterTable('routine_trainings', function(table) {
+    table.dropColumn('completed');
+  });
+};
